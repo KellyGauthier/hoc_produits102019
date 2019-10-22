683d4c212ae6a908e58b87e74295cb43a53e042e
KellyGauthier <laviolettek@gmail.com>
2019-10-22 11:41:01 +0200

    version refactorisée

--- newsletter_process.php
+++ newsletter_process.php
@@ -1,5 +1,9 @@
 <?php
+require_once 'src/Utils.php';
 session_start();
+
+$util = new Utils;
+
 var_dump ($_POST);
 
 // TODO: Insérer l'email en BDD
@@ -9,7 +13,7 @@ echo $_POST["email"];
 // TODO : vérifier que la clé notification est bien vide
 // sinon on écrase toutes les notifications précédentes
 // Donc, on voudra ajouter une nouvelle notification au tableau
-// Maos vérifier qu'il est défini, et s'il ne l'eat pas, le créer
+// Mais vérifier qu'il est défini, et s'il ne l'est pas, le créer
 $_SESSION["notifications"] = [
     'success' => [
         'Merci, votre email a bien été enregistré'
@@ -17,10 +21,7 @@ $_SESSION["notifications"] = [
     'info' => [
         'RGPD : Votre email ne sera pas utilisé pour de la publicité.'
     ]
-    
 ];
 
 // rediriger vers la page d'accueil
-// TODO : factoriser cette méthode dans une classe utilitaire
-header('Location: index.php');
-exit;
+$util->redirect('index.php');
