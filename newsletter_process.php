6424705be1b1c9002a39b6a2dd8a343fd65973d6
KellyGauthier <laviolettek@gmail.com>
2019-10-23 11:17:44 +0200

    file loader

--- newsletter_process.php
+++ newsletter_process.php
@@ -1,53 +1,41 @@
 <?php
 require_once 'vendor/autoload.php';
 session_start();
-
-
+use App\Config;
 use App\Utils;
-
 try {
-
-    if (!file_exists('config/db.ini') || !$dbConfig = parse_ini_file('config/db.ini')) {
-        throw new Exception("Une erreur est survenue lors du chargement du fichier de configuration");
-    }
-
-    $dsn = 'mysql:host=' . $dbConfig['host'] .
-        ';dbname=' . $dbConfig['name'] .
-        ';charset=' . $dbConfig['charset'];
-
-    $pdo = new PDO($dns, $dbConfig['user'], $dbConfig['password']);
+    $dbConfig = new Config('config/db.ini');
+    $dsn = 'mysql:host=' . $dbConfig->host .
+            ';dbname=' . $dbConfig->dbname .
+            ';charset=' . $dbConfig->charset;
+    $pdo = new PDO($dsn, $dbConfig->user, $dbConfig->password);
 } catch (Exception $ex) {
-    // TODO : générer une nouvelle notification d'erreur avant de rediriger vers la page d'accueil
+    // TODO: générer nouvelle notification d'erreur avant de rediriger vers la page d'accueil
     Utils::redirect('index.php');
 }
-
-if (empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-    // TODO : générer une notification d'erreur sur le format de l'adresse email
+if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    // TODO: générer une notification d'erreur sur le format de l'adresse email
     Utils::redirect('index.php');
 }
-
-$stmt = $pdo->prepare('INSERT INTO newsletter (email) VALUES (:email)');
-
+$stmt = $pdo->prepare("INSERT INTO newsletter (email) VALUES (:email)");
 $res = $stmt->execute([
-    'email' => $_POST["email"]
+    'email' => $_POST['email']
 ]);
-
-//TODO : générer notification selon réussite ou échec de la requete
-
-
-// enregistrer message dans la session
-// TODO : vérifier que la clé notification est bien vide
-// sinon on écrase toutes les notifications précédentes
-// Donc, on voudra ajouter une nouvelle notification au tableau
+var_dump($res);
+// TODO: générer notification selon réussite ou échec de la requête
+// Enregistrer un message dans la session
+// TODO: Vérifier que la clé notifications est bien vide
+// Sinon, on va écraser toutes les notifications précédentes
+// Donc globalement, on voudra ajouter une notification au tableau
 // Mais vérifier qu'il est défini, et s'il ne l'est pas, le créer
-$_SESSION["notifications"] = [
-    'success' => [
-        'Merci, votre email a bien été enregistré'
-    ],
-    'info' => [
-        'RGPD : Votre email ne sera pas utilisé pour de la publicité.'
-    ]
-];
-
+// $_SESSION['notifications'] = [
+//     'success' => [
+//         'Merci, votre email a bien été enregistré',
+//         'T bo'
+//     ],
+//     'info' => [
+//         'RGPD : Votre email ne sera pas divulgué pour de la publicité'
+//     ]
+// ];
 // rediriger vers la page d'accueil
-Utils::redirect('index.php');
+//Utils::redirect('index.php');
---
+++ src/FileLoader/AbstractFIleLoader.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\FileLoader;
+abstract class AbstractFileLoader
+{
+    protected $file;
+    public function __construct(string $file)
+    {
+        $this->file = $file;
+    }
+    abstract public function load();
+}
